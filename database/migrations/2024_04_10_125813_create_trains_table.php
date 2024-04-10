91fe6e80df17c8c531566af6d11427ae79edd1af
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-04-10 16:18:38 +0200

    completato voci del database

--- database/migrations/2024_04_10_125813_create_trains_table.php
+++ database/migrations/2024_04_10_125813_create_trains_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
             $table->timestamps();
         });
     }
--- database/migrations/2024_04_10_131335_add_columns_to_trains_table.php
+++ database/migrations/2024_04_10_131335_add_columns_to_trains_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('trains', function (Blueprint $table) {
-            //
+            $table->string('departure_station', 255);
+            $table->string('arrival_station', 200);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('code', 30);
+            $table->tinyInteger('carriage');
+            $table->boolean('on_schedule');
+            $table->boolean('cancelled');
         });
     }
 
@@ -22,7 +29,15 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('trains', function (Blueprint $table) {
-            //
+            $table->dropColumn('departure_station');
+            $table->dropColumn('arrival_station');
+            $table->dropColumn('departure_time');
+            $table->dropColumn('arrival_time');
+            $table->dropColumn('code');
+            $table->dropColumn('carriage');
+            $table->dropColumn('on_schedule');
+            $table->dropColumn('cancelled');
+            
         });
     }
 };
